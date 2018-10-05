d8c1999fd1de8d34c0300b181725c83ee7a5cee6
Andrea Berman <andrea.berman@slalom.com>
2018-10-05 11:06:00 -0400

    Check in of React 101 demo

---
+++ ReactWebProject/ReactWebProject/App_Start/BundleConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web;
+using System.Web.Optimization;
+using System.Web.Optimization.React; //Add this namespace 
+
+namespace ReactWebProject.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new BabelBundle("~/bundles/main").Include("~/Scripts/react/ReactWebAppScript.jsx"));
+        }
+    }
+}
---
+++ ReactWebProject/ReactWebProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ReactWebProject.MvcApplication" Language="C#" %>
