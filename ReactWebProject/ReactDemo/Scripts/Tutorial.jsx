3af15ad3e1a9d37664e289bee019d91f75af1da2
Andrea Berman <andrea.berman@slalom.com>
2018-10-17 16:41:49 -0400

    Added state change objects

--- ReactWebProject/ReactDemo/Scripts/Tutorial.jsx
+++ ReactWebProject/ReactDemo/Scripts/Tutorial.jsx
@@ -26,6 +26,32 @@ class CommentBox extends React.Component {
                 <h1>Comments</h1>
                 <CommentList />
                 <CommentForm />
+                <Hello message="my friend" />, 
+            </div>
+        );
+    }
+}
+
+class Hello extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            message: "my friend (from state)!"
+        };
+        this.updateMessage = this.updateMessage.bind(this);
+    }
+
+    updateMessage() {
+        this.setState({
+            message: "my friend (from changed state)!"
+        });
+    }   
+
+    render() {
+        return (
+            <div>
+                <h1>Hello {this.state.message}!</h1>
+                <button onClick={this.updateMessage}>Click me!</button>
             </div>
         );
     }
